347a6709ff739cd98487ccaf452da348c7e1b00f
Martin <martin.reisner@gmail.com>
2012-01-19 14:43:19 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RNos::Application.config.secret_token = '737ae9616f5112c755c3707d2d71814da1667222050ca8d88b9b4be27ec2526d525ec33db5b9f60c7dc1233f5a81c2e4a53b98270b0127545c7f374a606ed6d9'
